a223b0744f163f8e39cb94dc88e7bd04a38e9adb
valy012 <valy.ghorbani@gmail.com>
2016-12-26 23:33:18 +0430

    Update Start.lua

--- plugins/Start.lua
+++ plugins/Start.lua
@@ -13,7 +13,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'Welcome to BlackPlus!\n\n>To get a new BlackPlus group, contact a support group:\n https://telegram.me/joinchat/A2ETvD160xvsnwhJ3eQk0Q\n\nFor more information, check out our channels:\n\nChannel : @Black_CH\nSite : BlackPlus.ir \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @BlackPlus!'
+local text = 'Welcome to safroid!\n\n>To get a new safroid group, contact a support group:\n https://telegram.me/joinchat/A2ETvD160xvsnwhJ3eQk0Q\n\nFor more information, check out our channels:\n\nChannel : @safroid_group\nSite :  \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @safroid_group!'
 if matches [1] == 'start' then
 reply_msg(reply_id, text, ok_cb, false)
 end
