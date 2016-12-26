1ca560b60c05d0b9d277ffeb45cceb0f57e2ebb1
valy012 <valy.ghorbani@gmail.com>
2016-12-26 23:47:14 +0430

    Update InPm.lua

--- plugins/InPm.lua
+++ plugins/InPm.lua
@@ -220,7 +220,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "Welcome to BlackPlus!\n\n>To get a new BlackPlus group, Use /support command to get a support Link! Or Contact a Support Team With @BlackSupport_Bot\n\nFor more information, check out our channel:\n[@Black_CH] \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @BIackPlus!"
+		text = "Welcome to safroid!\n\n>To get a new safroid group, Use /support command to get a support Link! Or Contact a Support Team With @asafroiddd_bot\n\nFor more information, check out our channel:\n[@safroid_group] \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @safroid_group!"
      	return text
     end
 
